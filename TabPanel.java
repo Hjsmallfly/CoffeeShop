f9bc7cbb4dcec4ccfddccde20575467cafaf0766
stunwad <xiaofei_love20@163.com>
2015-05-20 22:47:35 +0800

    商品列表与信息与GUI关联起来了

--- TabPanel.java
+++ TabPanel.java
@@ -34,12 +34,12 @@ import exercise.product.Beverage;
 import exercise.resourcepath.ResourceFilePath;
 
 public class TabPanel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField txtAddMoreSugar;
+	private JTextField costTextBox;
+	private JTextField countTextBox;
+	private JTextField psTextBox;
 	private static String[] strs = new String[]{"Mocha","Expresso","Coffee","Others..."};
 	private static String[] billStrs = new String[]{"Mocha 1 x $9.9","Expresso 2 x $6.6","Coffee 3 x $1.0"};
-	private JTextField textField_2;
+	private JTextField totalCostTextBox;
 	private ButtonGroup sizeSelectBP = new ButtonGroup();
 	/**
 	 * Create the panel.
@@ -59,59 +59,66 @@ public class TabPanel extends JPanel {
 		add(list);
 		ArrayList<Beverage> bl = ResourceFilePath.readAllBeverage();
 		
+
+		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setBorder(new TitledBorder(null, "商品信息", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		infoPanel.setBounds(125, 41, 181, 248);
+		add(infoPanel);
+		infoPanel.setLayout(null);
+		
+		costTextBox = new JTextField("$100");
+		costTextBox.setEditable(false);
+		costTextBox.setText("0");
+		costTextBox.setBorder(new TitledBorder("单价"));
+		costTextBox.setBounds(10, 25, 161, 47);
+		infoPanel.add(costTextBox);
+		costTextBox.setColumns(10);
+		
+		JTextArea descriptionTextArea = new JTextArea();
+		descriptionTextArea.setForeground(Color.DARK_GRAY);
+		descriptionTextArea.setFont(new Font("微软雅黑", Font.PLAIN, 13));
+		descriptionTextArea.setText("商品描述信息...");
+		descriptionTextArea.setBounds(10, 82, 161, 123);
+		descriptionTextArea.setBorder(new TitledBorder("商品信息"));
+		infoPanel.add(descriptionTextArea);
+		
+		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(10, 41, 111, 248);
 		add(scrollPane_1);
-		JList productListBox = new ProductList(bl);
+		ProductList productListBox = new ProductList(bl);
+		productListBox.setCostTextBox(costTextBox);
+		productListBox.setDescriptionTextArea(descriptionTextArea);
 		scrollPane_1.setViewportView(productListBox);
 		
 		productListBox.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "商品", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(99, 108, 113)));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "商品信息", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(125, 41, 181, 248);
-		add(panel);
-		panel.setLayout(null);
-		
-		textField = new JTextField("$100");
-		textField.setEditable(false);
-		textField.setBorder(new TitledBorder("单价"));
-		textField.setBounds(10, 25, 161, 47);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JTextArea txtrHereIsThe = new JTextArea();
-		txtrHereIsThe.setForeground(Color.DARK_GRAY);
-		txtrHereIsThe.setFont(new Font("微软雅黑", Font.PLAIN, 13));
-		txtrHereIsThe.setText("商品描述信息...");
-		txtrHereIsThe.setBounds(10, 82, 161, 123);
-		txtrHereIsThe.setBorder(new TitledBorder("商品信息"));
-		panel.add(txtrHereIsThe);
 		
 		JButton editButton = new JButton("编辑");
 		editButton.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		editButton.setBounds(94, 215, 77, 23);
-		panel.add(editButton);
+		infoPanel.add(editButton);
 		
-		JButton btnNewitem = new JButton("新商品");
-		btnNewitem.setFont(new Font("微软雅黑", Font.PLAIN, 12));
-		btnNewitem.setBounds(10, 215, 74, 23);
-		panel.add(btnNewitem);
+		JButton newProductButton = new JButton("新商品");
+		newProductButton.setFont(new Font("微软雅黑", Font.PLAIN, 12));
+		newProductButton.setBounds(10, 215, 74, 23);
+		infoPanel.add(newProductButton);
 		
 		JComboBox comboBox = new JComboBox(new Object[]{});
 		comboBox.setEditable(true);
 		comboBox.setBounds(-250, 0, 224, 21);
-		panel.add(comboBox);
+		infoPanel.add(comboBox);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "订单", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.setBounds(316, 41, 287, 248);
-		add(panel_1);
-		panel_1.setLayout(null);
+		JPanel billPanel = new JPanel();
+		billPanel.setBorder(new TitledBorder(null, "订单", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		billPanel.setBounds(316, 41, 287, 248);
+		add(billPanel);
+		billPanel.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 22, 154, 164);
-		panel_1.add(scrollPane);
+		billPanel.add(scrollPane);
 		
 		JList billListBox = new JList(billStrs);
 		scrollPane.setViewportView(billListBox);
@@ -120,82 +127,83 @@ public class TabPanel extends JPanel {
 		JLabel lblCount = new JLabel("数量:");
 		lblCount.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblCount.setBounds(164, 22, 54, 22);
-		panel_1.add(lblCount);
+		billPanel.add(lblCount);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(203, 23, 74, 21);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		countTextBox = new JTextField();
+		countTextBox.setBounds(203, 23, 74, 21);
+		billPanel.add(countTextBox);
+		countTextBox.setColumns(10);
 		
 		JLabel lblPs = new JLabel("备注:");
 		lblPs.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblPs.setBounds(164, 89, 54, 22);
-		panel_1.add(lblPs);
+		billPanel.add(lblPs);
 		
-		txtAddMoreSugar = new JTextField();
-		txtAddMoreSugar.setFont(new Font("微软雅黑", Font.PLAIN, 12));
-		txtAddMoreSugar.setText("多加糖");
-		txtAddMoreSugar.setColumns(10);
-		txtAddMoreSugar.setBounds(203, 90, 74, 21);
-		panel_1.add(txtAddMoreSugar);
+		psTextBox = new JTextField();
+		psTextBox.setFont(new Font("微软雅黑", Font.PLAIN, 12));
+		psTextBox.setText("多加糖");
+		psTextBox.setColumns(10);
+		psTextBox.setBounds(203, 90, 74, 21);
+		billPanel.add(psTextBox);
 		
 		JLabel lblCost = new JLabel("订单价格:");
 		lblCost.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblCost.setBounds(164, 121, 89, 15);
-		panel_1.add(lblCost);
+		billPanel.add(lblCost);
 		
 		JLabel label = new JLabel("$9.9");
 		label.setForeground(Color.RED);
 		label.setBounds(233, 121, 44, 15);
-		panel_1.add(label);
+		billPanel.add(label);
 		
 		JLabel lblIncon = new JLabel("收款:");
 		lblIncon.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblIncon.setBounds(164, 146, 54, 15);
-		panel_1.add(lblIncon);
+		billPanel.add(lblIncon);
 		
 		JLabel label_1 = new JLabel("$9.9");
 		label_1.setForeground(Color.RED);
 		label_1.setBounds(233, 146, 44, 15);
-		panel_1.add(label_1);
+		billPanel.add(label_1);
 		
 		JLabel lblDiff = new JLabel("找出:");
 		lblDiff.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblDiff.setBounds(164, 171, 54, 15);
-		panel_1.add(lblDiff);
+		billPanel.add(lblDiff);
 		
 		JLabel label_2 = new JLabel("$0.0");
 		label_2.setForeground(Color.RED);
 		label_2.setBounds(233, 171, 44, 15);
-		panel_1.add(label_2);
+		billPanel.add(label_2);
 		
 		JLabel lblPrize = new JLabel("总价格:");
 		lblPrize.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		lblPrize.setBounds(164, 54, 54, 22);
-		panel_1.add(lblPrize);
+		billPanel.add(lblPrize);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(203, 54, 74, 21);
-		panel_1.add(textField_2);
+		totalCostTextBox = new JTextField();
+		totalCostTextBox.setColumns(10);
+		totalCostTextBox.setBounds(203, 54, 74, 21);
+		billPanel.add(totalCostTextBox);
 		
 		JButton btnNewButton = new JButton("确认订单");
 		btnNewButton.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		btnNewButton.setBounds(10, 215, 267, 23);
-		panel_1.add(btnNewButton);
+		billPanel.add(btnNewButton);
 		
 		JRadioButton smallJRadio = new JRadioButton("小");
 		smallJRadio.setBounds(20, 192, 57, 23);
-		panel_1.add(smallJRadio);
+		billPanel.add(smallJRadio);
 		
 		JRadioButton middleJRadio = new JRadioButton("中");
 		middleJRadio.setBounds(79, 192, 54, 23);
-		panel_1.add(middleJRadio);
+		billPanel.add(middleJRadio);
 		
 		JRadioButton bigJRadio = new JRadioButton("大");
 		bigJRadio.setBounds(139, 192, 73, 23);
-		panel_1.add(bigJRadio);
-
+		billPanel.add(bigJRadio);
+		
+		//这是添加到按钮组上
 		sizeSelectBP.add(bigJRadio);
 		sizeSelectBP.add(smallJRadio);
 		sizeSelectBP.add(middleJRadio);
--- customizegui/ProductList.java
+++ customizegui/ProductList.java
@@ -4,13 +4,34 @@ import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import exercise.product.Production;
 
+/**
+ * 用来显示产品的Jlist
+ * @author STU_nwad
+ *
+ */
 public class ProductList extends JList<Production> {
 	private DefaultListModel productionListModel = new DefaultListModel(); //列表模型
 	
 	private ArrayList productionArray;
+	
+	private JTextField costTextBox; //用于显示价格的TextBox
+	
+	private JTextArea descriptionTextArea; //用于显示商品信息的TextArea
+	
+	public void setCostTextBox(JTextField t){
+		costTextBox = t;
+	}
+	
+	public void setDescriptionTextArea(JTextArea t){
+		descriptionTextArea = t;
+	}
 	
 	/**
 	 * 
@@ -26,10 +47,23 @@ public class ProductList extends JList<Production> {
 			productionListModel.addElement(o);
 		}
 		setModel(productionListModel); //设置模型
+		
+		addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (getSelectedIndex() != -1){
+					costTextBox.setText((( Production)productionArray.get(getSelectedIndex())).getCost() + " ");
+					descriptionTextArea.setText((( Production)productionArray.get(getSelectedIndex())).description());
+				}
+			}
+		});
+		
 	}
 	
 	public ProductList() {
 		
 	}
+	
 	
 }
